4b18beee593a02939121b8e367687b6d2d3b2c1b
Jeff Cogswell <jeffcogs@gmail.com>
2022-09-27 14:37:24 -0400

    Updates

---
+++ DapperConsole/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper.Contrib.Extensions;
+
+namespace DapperConsole
+{
+	[Table("employee")]
+	public class Employee
+	{
+		[Key]
+		public int id { get; set; }
+		public string firstname { get; set; }
+		public string lastname { get; set; }
+		public string phone { get; set; }
+		public string email { get; set; }
+		public string department { get; set; }
+		public override string ToString()
+		{
+			return $"{id} {firstname} {lastname} {phone} {email} {department}";
+		}
+	}
+}
---
+++ DapperConsole/Program.cs
@@ -0,0 +1,40 @@
+﻿using Dapper;
+using Dapper.Contrib.Extensions;
+using DapperConsole;
+using MySql.Data.MySqlClient;
+
+var db = new MySqlConnection("Server=127.0.0.1;Database=business;Uid=root;Pwd=abc123;");
+List<Employee> emps = db.GetAll<Employee>().ToList();
+
+foreach (Employee emp in emps)
+{
+	Console.WriteLine(emp);
+}
+
+// Let's add a new employee
+// Leave out the id. Let the database create that for us.
+
+//Employee e1 = new Employee() { firstname = "Jeff", lastname = "Cogswell",
+//	phone = "517-111-2222", email = "jeff@google.com", department = "ACCT" };
+//db.Insert(e1);
+
+// Let's delete the one with id 28
+// db.Delete(new Employee() { id = 28 });
+
+Console.WriteLine("Let's just read a single one, by ID. We'll do id 5");
+Employee e2 = db.Get<Employee>(5);
+Console.WriteLine(e2);
+
+// Now let's change the spelling from Emily to Emilie.
+
+//e2.firstname = "Emilie";
+//db.Update<Employee>(e2);
+
+Console.WriteLine();
+Console.WriteLine("Let's get all employeees who work in the IT department");
+
+List<Employee> emps2 = db.Query<Employee>("select * from employee where department = 'IT'").ToList();
+foreach (Employee emp in emps2)
+{
+	Console.WriteLine(emp);
+}
